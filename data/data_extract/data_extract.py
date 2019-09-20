277ccb66f9f3a3d4ae9a231611cf99261e69d271
shine <263397787Q>
2019-09-20 17:25:22 +0800

    定时任务导出数据到指定文件夹

---
+++ data/data_extract/data_extract.py
@@ -0,0 +1,73 @@
+import pymssql
+import pandas as pd
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import uuid
+import os
+import json
+import schedule
+import logging
+import logging.handlers as handlers
+
+
+#初始化日志
+logging.basicConfig()
+mylog = logging.getLogger('mylog')
+mylog.setLevel(logging.DEBUG)
+formater = logging.Formatter('%(asctime)s - %(filename)s[line:%(lineno)d] %(levelname)s %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
+logfilehandler = handlers.TimedRotatingFileHandler("logs/myapp.log", when='D', interval=1, backupCount=30, encoding='utf-8')
+logfilehandler.suffix = "%Y-%m-%d.log"
+logfilehandler.setFormatter(formater)
+mylog.addHandler(logfilehandler)
+
+def read_script():
+    with open("script.sql",'r',encoding='UTF-8-sig') as sql_script:
+        sql =sql_script.read()
+    return sql
+
+def read_config():
+    with open("config.json",'rb') as json_file:
+        config = json.load(json_file)
+    return config
+
+def update_config(config):
+    with open("config.json", 'w') as json_file:
+        json.dump(config, json_file, indent=2)
+    return None
+
+config = read_config()
+
+def extract_data():
+    export_path = config["export_path"] #配置的导出主路径
+    if not os.path.exists(export_path):
+        os.mkdir(export_path)
+    save_path = export_path + "/" + datetime.now().strftime('%Y-%m') #每月的导出路径
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
+    conn = pymssql.connect(config['server'], config['user'], config['password'], config['database'])
+    pre_date = datetime.now() - relativedelta(months=-1)
+    start_date = pre_date.strftime('%Y-%m')+"-1"
+    end_date = pre_date.strftime('%Y-%m') + "-31"
+    sql = read_script().format(start_date,end_date)  #读取sql语句脚本
+    mylog.info('开始读取数据库')
+    df = pd.read_sql(sql,conn) #将数据库脚本读取成dataFrame
+    writer = pd.ExcelWriter(save_path +"/"+ str(uuid.uuid1()) + ".xlsx")
+    df.to_excel(writer,sheet_name = "data") #导出成excel
+    writer.save()
+    return True
+
+def schedule_job():
+    mylog.info("任务正常执行")
+    if datetime.now().day == int(config['export_day']): #每月设定日期执行
+        mylog.info("开始执行导出")
+        try:
+            extract_data()
+        except Exception as ex:
+            mylog.error(ex)
+        mylog.info("完成导出数据操作")
+
+if __name__=="__main__":
+    mylog.info("服务启动")
+    schedule.every().day.at(config['export_time']).do(schedule_job)#设置计划，每天设置的时间点
+    while True:
+        schedule.run_pending()
